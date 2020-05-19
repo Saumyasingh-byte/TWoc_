fd8b867630584a74bf8c3dc594d3fca2a670799e
Saumyasingh-byte <64596782+Saumyasingh-byte@users.noreply.github.com>
2020-05-19 18:23:44 +0530

    Update problem-5.py

--- problem-5.py
+++ problem-5.py
@@ -1,5 +1,5 @@
-n = int(input("Enter the Value of N: "))
-for i in range(n):
-    print("* " * (n - i) +"    " * i +" *" * (n - i))
-for i in range(1,n + 1):
-    print("* " * i +"    " * (n - i) +" *" * i)
+N = int(input("Enter the Value of N: "))
+for i in range(N):
+    print((str(N - i) + "*") * (N - 1 - i) + str(N - i))
+for i in range(1,N + 1):
+    print((str(i) + "*") * (i - 1) + str(i))
