03a77563b23ab9a690c16a229213945f6d074d35
Saumyasingh-byte <64596782+Saumyasingh-byte@users.noreply.github.com>
2020-05-19 18:17:04 +0530

    problem-5.py

---
+++ problem-5.py
@@ -0,0 +1,9 @@
+p1=int(input(" eneter the runs of player 1:"))
+p2=int(input(" eneter the runs of player 2:"))
+p3=int(input(" eneter the runs of player 3:"))
+srp1=(p1/60)*100
+srp2=(p2/60)*100
+srp3=(p3/60)*100
+print(" strick rate of player 1 is {} , player 2 is {} and player 3 is {}".format(srp1,srp2,srp3))
+print(" runs after playing 60 more balls is player 1 :{} ,player 2 :{} and player 3: {}".format(p1*2,p2*2,p3*2))
+print(" max sixes of player 1 :{} , player 2:{} and player 3:{}".format(p1//6,p2//6,p3//6))
